ad17d4107ab0d23bdb059b0bdea273c208ef3ec0
Jay Logan <jaylogan@jaylogan.com>
2025-04-10 18:56:53 -0400

    Remove dependency of tailwind cdn

--- index.php
+++ index.php
@@ -32,17 +32,11 @@
 
     <!-- Fonts & Frameworks -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;500;600;700;800;900&display=swap" rel="stylesheet">
-    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <style>
-    * {
-        font-family: "Inter", sans-serif;
-    }
-    ::-webkit-scrollbar {
-        display: none;
-    }
-    </style>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/styles.css">
+
 </head>
 
 <body class="h-full text-base-content">
