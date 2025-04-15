350f24322040216375ef270100a02aab2e9e08db
Jay Logan <jaylogan@jaylogan.com>
2025-04-15 15:13:47 -0400

    New CSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-// tailwind.config.js
 module.exports = {
+  mode: "jit",
   content: [
-    "./index.php",
-    "./**/*.php",      // Include all PHP files in subdirectories (just in case)
-    "./**/*.html",     // Optional: if you also use HTML
-    "./**/*.js",       // Optional: if Tailwind classes appear in JS
+    "./src/**/*.{html,js}",
+    "./index.html"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    
+  ]
 }
